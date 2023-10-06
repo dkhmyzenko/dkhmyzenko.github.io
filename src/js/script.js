8e5435df3be76b305deb71a0780e99491e889baf
Denis <dendyzet@gmail.com>
2023-10-06 12:51:50 +0300

    toggler&tabs

--- src/js/script.js
+++ src/js/script.js
@@ -78,3 +78,75 @@ slider.addEventListener('touchend', e => {
         return false;
     }
 });
+
+//toggler
+
+const more = document.querySelectorAll('.catalog-elem__link'),
+      back = document.querySelectorAll('.catalog-elem__list-link'),
+      contentItem = document.querySelectorAll('.catalog-elem__content'),
+      listItem = document.querySelectorAll('.catalog-elem__list'),
+      wrapper = document.querySelectorAll('.catalog-elem__wrapper');
+
+
+contentItem.forEach( elem => {
+    elem.classList.add('catalog-elem__content_active');
+});
+
+function showList(i) {
+    contentItem[i].classList.remove('catalog-elem__content_active');
+    listItem[i].classList.add('catalog-elem__list_active'); 
+}
+
+function showContent(i) {
+    listItem[i].classList.remove('catalog-elem__list_active');
+    contentItem[i].classList.add('catalog-elem__content_active'); 
+}
+
+more.forEach( function (elem, i) {
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        showList(i);
+    });
+});
+
+back.forEach(function(link, i) {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        showContent(i);
+    });
+});
+
+//tabs
+
+const tabsHeader = document.querySelectorAll('.catalog__tab'),
+      tabsParent = document.querySelector('.catalog__tabs'),
+      tabsContent = document.querySelectorAll('.catalog__elems');
+
+    function hideTabsContent() {
+        tabsHeader.forEach((item) => {
+            item.classList.remove('catalog__tab_active');
+        });
+        tabsContent.forEach(item => {
+            item.classList.add('catalog__elems_hidden');
+        });
+    }
+
+    function showTabsContent(i = 1) {
+        tabsHeader[i].classList.add('catalog__tab_active');
+        tabsContent[i].classList.remove('catalog__elems_hidden');
+    }
+
+
+    tabsHeader.forEach((elem,i) => {
+        elem.addEventListener('click', e => {
+            if (e.target && e.target.classList.contains('catalog__tab')){
+                hideTabsContent();
+                showTabsContent(i); 
+            } else {
+                hideTabsContent();
+                showTabsContent(i); 
+            }
+        });
+    });
+
+    tabsHeader[0].click();
